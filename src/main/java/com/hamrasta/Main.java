e602c0d0b0048bc53681af43be8e0c44770acace
malaki12003 <malaki12003@gmail.com>
2020-05-16 22:35:42 +0430

    add mvn wrapper

--- src/main/java/com/hamrasta/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hamrasta;
-
-import com.hamrasta.sms_tools.util.SmsUtil;
-
-public class Main  {
-    public static void main(String[] args) {
-        SmsUtil.send("09124598022", "testF");
-    }
-}
